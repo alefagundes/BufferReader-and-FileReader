957acf6af8ec0c8310c1ffc03952351e2976cef0
alefagundes <alefagundes2000.af@gmail.com>
2021-08-03 22:20:12 -0300

    Short Solution

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -9,34 +9,15 @@ public class Program {
 	public static void main(String[] args) {
 		String path = "C:\\Users\\Pichau\\eclipse-workspace\\arquivo\\arq\\teste.txt";
 		
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			String line = br.readLine();
-			
-			while(line != null) {
+			while(line!=null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-		}
-		catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if(br != null) {
-					br.close();
-				}
-				if(fr !=null) {
-					fr.close();
-				}
-				}catch(IOException e) {
-					e.printStackTrace();
-				}
+			
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
 		}
 
 	}
